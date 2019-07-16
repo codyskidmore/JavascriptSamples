e4eb5de9e46d425108915422d256efe16a9f814e
Cody Skidmore <cody.skidmore@vueworks.com>
2019-07-16 13:36:58 -0400

    Math operations and math object demo. Skipped basics.

---
+++ app.js
@@ -0,0 +1,21 @@
+// Math operations
+console.log(10 * 1.25); // 12.5
+
+// Math Object
+
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math.random()); // random decimal number
+console.log("random whole number between 1 - 20.");
+console.log(Math.floor(Math.random() * 20 + 1)); // random whole number
+console.log(Math.round(10.5)); // 11
+console.log(Math.round(10.4)); // 10
+console.log(Math.round(10.8)); // 11
+console.log(Math.ceil(10.4)); //11
+console.log(Math.floor(10.5)); //10
+console.log(Math.sqrt(4)); // 2
+console.log(Math.abs(-4)); // 4
+console.log(Math.pow(4,2)); // 16
+console.log(Math.min(4,2,5)); // 2
+console.log(Math.max(4,2,5)); // 5
+
