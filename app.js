5b7125275d61ef16d629ec62770f7ba701b2efa6
Cody Skidmore <cody.skidmore@vueworks.com>
2019-07-17 11:31:07 -0400

    1st round of samples.

---
+++ app.js
@@ -0,0 +1,11 @@
+const numbers = [12,43,1,5,16,2];
+// const numbers2 = new Array(12,43,1,5,16,2,'me', new Date(), {a:1, b:2});
+
+console.log([12,43,1,5,16,2,'me', new Date()]);
+console.log(new Array(12,43,1,5,16,2,'me', new Date(), {a:1, b:2}));
+
+console.log(numbers.length);
+console.log(Array.isArray(numbers));
+
+
+
