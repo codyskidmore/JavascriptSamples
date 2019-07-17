4e7d61b8cc14c0fd487e10568f7894ecd4f7e56a
Cody Skidmore <cody.skidmore@vueworks.com>
2019-07-17 12:43:20 -0400

    Checkpoint before continuing with adding samples.

--- app.js
+++ app.js
@@ -4,8 +4,24 @@ document.querySelector("h1").innerHTML = title;
 
 const person = {
   firstName: "Bob",
-  lastName: "Jones"
+  lastName: "Jones",
+  age: 40,
+  emailAddress: 'bob@example.com',
+  hobbies: ['Photography','Cooking'],
+  adderss: {
+    street: '504 West Hollywood Blvd',
+    city: 'Hollywood',
+    state: 'California'
+  },
+  getBirthYear: function(){
+    return new Date().getFullYear() - this.age;
+  }
 };
 
-console.log(person);
-console.log(person.firstName);
+console.log(person); // see output
+console.log(person.firstName); // Bob
+// also works but not recommended
+//console.log(person['firstName']);
+console.log(person.getBirthYear()); // March
+console.log(person.adderss.state); // California
+
