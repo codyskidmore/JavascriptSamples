33f42e1d86b5461b6ded63a0e5442276414880fd
DESKTOP-4V1NKRS\Cody Skidmore <nospam@skidmore.us>
2019-07-19 15:32:07 -0400

    Added a swtich example that also includes one possible way to declare constants to use in code.

--- app.js
+++ app.js
@@ -2,3 +2,40 @@ const title = "Switch Statements";
 document.title = title;
 document.querySelector("h1").innerHTML = title;
 
+class Colors {
+    static get RED(){
+        return {
+            name: 'red',
+            hex: '#f00'
+        }
+    }
+    static get BLUE(){
+        return {
+            name: 'blue',
+            hex: '#00f'
+        }
+    }
+    static get GREEN(){
+        return {
+            name: 'green',
+            hex: '#0f0'
+        }
+    }
+}
+
+const color = Colors.RED.name;
+
+switch(color)
+{
+    case Colors.RED.name:
+        console.log('Color is red');
+        break;
+    case Colors.BLUE.name:
+        console.log('Color is blue');
+        break;
+    case Colors.GREEN.name:
+        console.log('Color is green');
+        break;
+    default:
+        console.log('Unknown color');
+}
