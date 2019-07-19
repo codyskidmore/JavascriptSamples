e47357023b46d4c4ed8f01901b019a16993e9c23
DESKTOP-4V1NKRS\Cody Skidmore <nospam@skidmore.us>
2019-07-19 16:39:03 -0400

    Added examples for function declarations & expressions.

--- app.js
+++ app.js
@@ -1,4 +1,40 @@
-const title = "";
+const title = "Function Declarations & Expressions";
 document.title = title;
 document.querySelector("h1").innerHTML = title;
 
+// Function Declaration
+
+function greet(fullName = 'John Doe')
+{
+    return `Hello ${fullName}.`;
+}
+
+console.log(greet('Bob Jones'));
+console.log(greet());
+
+// Function Expression
+const square = function(x = 1){
+    return x*x;
+};
+
+console.log(square(4));
+
+const iifeParam = 'test';
+// Immediately invokable function expressions => IIFEs
+(function(testParam){
+    console.log(`IIFE ran with value ${testParam}`);
+})(iifeParam); // Trailing parenthesis required to execute the IIFE. Param is optional.
+
+// Property Method
+const todo = {
+    add: function(){
+        console.log('do work');
+    }
+}
+todo.add();
+
+todo.delete = function(){
+    console.log('deleted work');
+}
+
+todo.delete();
