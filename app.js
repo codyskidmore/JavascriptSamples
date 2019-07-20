5d6f92bdc04242827f3d914afe6b145ea3306752
DESKTOP-4V1NKRS\Cody Skidmore <nospam@skidmore.us>
2019-07-20 08:26:35 -0400

    Illustrated the dangers of scope in JavaScript and reasons you should use let/const instead of var.

--- app.js
+++ app.js
@@ -2,3 +2,35 @@ const title = "Block Scope With let & const";
 document.title = title;
 document.querySelector("h1").innerHTML = title;
 
+// Global scope
+var a = 1; 
+let b  = 2;
+const c = 3;
+
+for(let a = 0; a < 10; a++){
+    console.log(`Loop scope of a: ${a}`);
+}
+// var a unaffected because loop declared 'a' using 'let' in inner scope.
+// declaring for(var a...) would change the globally scoped var a. 
+console.log('Global Scope', a, b, c); 
+
+function test(){
+    var a = 4; 
+    let b  = 5;
+    const c = 6;
+    console.log('Function Scope', a, b, c);
+}
+
+test();
+
+console.log('Global Scope', a, b, c); // var a unaffected.
+
+if (true){
+    // block scope
+    var a = 4;  // collision
+    let b  = 5;
+    const c = 6;
+    console.log('Block Scope', a, b, c);
+}
+
+console.log('Global Scope', a, b, c);  // var a was changed by block scope.
