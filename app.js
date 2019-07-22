38fa8e95226e2d45159a98c5974d4f503b1fe5d9
Cody Skidmore <cody.skidmore@vueworks.com>
2019-07-22 12:55:17 -0400

    Added samples demonstrating how to get a collection of elements from the DOM.

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-const title = "";
+const title = "DOM Selectors for Multiple Elements";
 document.title = title;
-document.querySelector("h1").innerHTML = title;
 
+const items = document.getElementsByTagName('li');
+console.log(items[0]);
+
+document.querySelectorAll('li:nth-child(odd').forEach(function(li, index){
+    li.style.background = '#9faccc';
+    li.style.color = '#fff'
+    li.textContent = `${index} odd task`;
+});
+
+
+document.querySelectorAll('li:nth-child(even').forEach(function(li, index){
+    li.style.background = '#ad9fcc';
+    li.style.color = '#fff'
+    li.textContent = `${index} even task`;
+});
