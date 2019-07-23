35d48e10bb9c224c69934ef332f328102c279590
Cody Skidmore <cody.skidmore@vueworks.com>
2019-07-23 08:04:13 -0400

    Added samples illustrating now to inject new DOM elements.

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 const title = "Creating Elements";
 document.title = title;
 
+const li = document.createElement('li');
+li.className = 'collection-item';
+li.id = 'new-item';
+li.setAttribute('title','New Item');
+li.appendChild(document.createTextNode('New List Item'));
+const anchor = document.createElement('a');
+anchor.className = 'delete-item secondary-content';
+anchor.innerHTML = '<i class="fa fa-remove"></i>';
+anchor.href = '#';
+
+li.appendChild(anchor);
+
+console.log(li);
+
+const ul = document.querySelector('ul');
+ul.appendChild(li); // see browser output
