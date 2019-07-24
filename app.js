79c80542ec1a4cfa6b13fe6da7f28f818947b2f4
DESKTOP-4V1NKRS\Cody Skidmore <nospam@skidmore.us>
2019-07-24 09:00:09 -0400

    Added references to const.js so I can start reusing the code by default in each sample.

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 const title = "Event Bubbling & Delegation";
 document.title = title;
+
+const cardTitle = document.querySelector('#card-title');
+
+function runClickEvent(e){
+    console.log('card title clicked');
+}
+
+cardTitle.addEventListener(MouseEvents.Click(), runClickEvent);
+
+//console.log(cardTitle);
